284b3e269c47b4340834824cb0801e130b66aa58
Marcel Julian <marceljulian2047@gmail.com>
2019-04-20 16:26:12 +0700

    Minor speed update.

--- src/objects/MomonSoldier0.java
+++ src/objects/MomonSoldier0.java
@@ -7,7 +7,7 @@ public class MomonSoldier0 extends MomonParent{
 	public MomonSoldier0() {
 		super();
 		hp = 400;
-		speed = 100;
+		speed = 90;
 		gold = 5;
 		img = Tile.getMomons().get(0);
 	}
--- src/objects/MomonTank.java
+++ src/objects/MomonTank.java
@@ -7,7 +7,7 @@ public class MomonTank extends MomonParent{
 	public MomonTank() {
 		super();
 		hp = 2500;
-		speed = 120;
+		speed = 100;
 		gold = 25;
 		img = Tile.getMomons().get(3);
 		
